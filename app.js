c0a765f75427c98c78985866397eaea582a95d28
JINGWEN LIN <yaysa903@gmail.com>
2022-02-10 23:31:56 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const exphbs = require('express-handlebars').create({ defaultLayout: 'main', extname: '.hbs' })
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/url-data')
+const db = mongoose.connection
+
+db.on('error', () => console.log('mongoDB error'))
+db.once('open', () => console.log('mongoDB connection'))
+
 const app = express()
 const port = 3000
 
