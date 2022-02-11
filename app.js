cf3a7bd29c83c25805453db97be3fadd09bb0b7d
JINGWEN LIN <yaysa903@gmail.com>
2022-02-12 00:09:25 +0800

    refactor: modify route express

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express')
-const validUrl = require('valid-url')
-const randomString = require('random-string')
 const exphbs = require('express-handlebars').create({ defaultLayout: 'main', extname: '.hbs' })
-const URL = require('./models/URL.js')
+const routes = require('./routes')
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/url-data')
@@ -17,43 +15,8 @@ const port = 3000
 app.engine('hbs', exphbs.engine)
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
+app.use(routes)
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
-
-app.post('/', (req, res) => {
-  const input_url = req.body.url
-  const randomCode = randomString({
-    length: 5,
-    numeric: true,
-    letters: true,
-    special: false,
-  })
-
-  // verify url
-  // if yes, check existed data in database
-  if (validUrl.isUri(input_url)) {
-    return URL.findOne({ orignal_url: input_url })
-      .then(url => url ? url : URL.create({ orignal_url: input_url, shorten_url: randomCode }))
-      .then(url => res.render('index', { url: url.shorten_url }))
-      .catch(error => console.log(error))
-  } else {
-    // if no, return error message
-    const errorMessage = 'Please input valid url'
-    res.render('error_page', { errorMessage })
-  }
-})
-
-app.get('/:random_code', (req, res) => {
-  const randomCode = req.params.random_code
-  return URL.findOne({ shorten_url: randomCode })
-    .then(url => res.status(301).redirect(url.orignal_url))
-    .catch(error => {
-      console.log(error)
-      res.render('error_page', { status: 500, errorMessage: error.message })
-    })
-})
 
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`)
---
+++ routes/modules/urls.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const router = express.Router()
+const validUrl = require('valid-url')
+const randomString = require('random-string')
+const URL = require('../../models/url')
+
+router.post('/', (req, res) => {
+  const input_url = req.body.url
+  const randomCode = randomString({
+    length: 5,
+    numeric: true,
+    letters: true,
+    special: false,
+  })
+
+  // verify url
+  // if yes, check existed data in database
+  if (validUrl.isUri(input_url)) {
+    return URL.findOne({ orignal_url: input_url })
+      .then(url => url ? url : URL.create({ orignal_url: input_url, shorten_url: randomCode }))
+      .then(url => res.render('index', { url: url.shorten_url }))
+      .catch(error => console.log(error))
+  } else {
+    // if no, return error message
+    const errorMessage = 'Please input valid url'
+    res.render('error_page', { errorMessage })
+  }
+})
+
+
+
+module.exports = router 
