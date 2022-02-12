b3d052359327ab0db6b8ab4091d99368c7acb243
JINGWEN LIN <yaysa903@gmail.com>
2022-02-12 14:29:57 +0800

    fix: modify addEventListener function

--- public/javascripts/validation_form.js
+++ public/javascripts/validation_form.js
@@ -1,10 +1,11 @@
 const form = document.querySelector('#form')
 
-form.addEventListener('submit', function onSubmitted(event) {
-  console.log(1)
-  if (!form.checkValidity()) {
-    event.preventDefault()
-    event.stopPropagation()
-  }
-  form.classList.add('was-validated')
-})
+if (form) {
+  form.addEventListener('submit', function onSubmitted(event) {
+    if (!form.checkValidity()) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+    form.classList.add('was-validated')
+  })
+}
