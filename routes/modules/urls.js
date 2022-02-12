a29d2981e6ac5bd4524a5df2a7497c1c04f0b77b
JINGWEN LIN <yaysa903@gmail.com>
2022-02-12 13:56:47 +0800

    fix: avoid double randomCode

--- routes/modules/urls.js
+++ routes/modules/urls.js
@@ -6,12 +6,34 @@ const URL = require('../../models/url')
 
 router.post('/', (req, res) => {
   const input_url = req.body.url
-  const randomCode = randomString({
+  let randomCode = randomString({
     length: 5,
     numeric: true,
     letters: true,
     special: false,
   })
+
+  // create empty collection
+  const urlCollection = []
+
+  // find all data and push in urlCollection
+  URL.find({})
+    .then(url => {
+      for (let i = 0; i < url.length; i++) {
+        urlCollection.push(url[i].shorten_url)
+      }
+      // check same randomCdoe in database
+      // when true, generate randomCode again
+      while (urlCollection.includes(randomCode)) {
+        randomCode = randomString({
+          length: 5,
+          numeric: true,
+          letters: true,
+          special: false,
+        })
+      }
+    })
+    .catch(error => console.log(error))
 
   // verify url
   // if yes, check existed data in database
@@ -20,14 +42,13 @@ router.post('/', (req, res) => {
       .then(url => url ? url : URL.create({ orignal_url: input_url, shorten_url: randomCode }))
       .then(url => res.render('index', { url: url.shorten_url }))
       .catch(error => console.log(error))
-  } 
-  // else {
-  //   // if no, return error message
-  //   const errorMessage = 'Please input valid url'
-  //   res.render('error_page', { errorMessage })
-  // }
+  }
+  else {
+    // if no, return error message
+    const errorMessage = 'Please input valid url'
+    res.render('error_page', { errorMessage })
+  }
 })
-
 
 
 module.exports = router 
