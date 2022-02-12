eddc0aa1f8f08bf7a8a4372c79a825a935ebbcc2
JINGWEN LIN <yaysa903@gmail.com>
2022-02-12 14:13:53 +0800

    fix: random_code routes

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,8 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const urls = require('./modules/urls')
-const shorten_url = require('./modules/shorten_url')
 
 router.use('/', home)
 router.use('/urls', urls)
-router.use('/:random_code', shorten_url)
 
 module.exports = router 
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const URL = require('../../models/url')
 
 router.get('/', (req, res) => {
   res.render('index')
+})
+
+router.get('/:random_code', (req, res) => {
+  const randomCode = req.params.random_code
+  return URL.findOne({ shorten_url: randomCode })
+    .then(url => res.status(301).redirect(url.orignal_url))
+    .catch(error => {
+      console.log(error)
+      res.render('error_page', { status: 500, errorMessage: error.message })
+    })
 })
 
 module.exports = router 
